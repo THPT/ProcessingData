e81981035d421fec6751777935f36f251ba5f041
Nikk <phuc.nguyen@go1.com>
2016-11-25 22:21:38 +0700

    Implement hot swap

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Import from MySQL to Hive using Sqooq:
 ./bin/sqoop import --connect jdbc:mysql://127.0.0.1/svcdb --username grok --password grok --table selling_store  --hive-overwrite --hive-import  --hive-drop-import-delims
 
 ./bin/sqoop import --connect jdbc:mysql://127.0.0.1/svcdb --username grok --password grok --table product  --hive-overwrite --hive-import  
---hive-drop-import-delims
+--hive-drop-import-delims -m 1
 
 ./bin/sqoop import --connect jdbc:mysql://127.0.0.1/svcdb --username grok --password grok --table video  --hive-overwrite --hive-import  --hive-drop-import-delims -m 1
 ```
@@ -22,4 +22,16 @@ Import from MySQL to Hive using Sqooq:
 Run:
 ```
 mvn clean && mvn compile && mvn package && spark-submit --class ProcessingData target/ProcessingData-0.0.1-SNAPSHOT.jar
+```
+
+Create grok postgre database and run this sql:
+
+https://github.com/THPT/generate_data/blob/master/sql/init.sql
+
+# Troubleshoot:
+
+```
+Sqoop: class ... not found
+
+=> Add --bindir [lib_location] to sqoop import command (need to run twice, first run compile the class)
 ```
--- src/ProcessingData.java
+++ src/ProcessingData.java
@@ -2,6 +2,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.spark.api.java.function.ForeachFunction;
 import org.apache.spark.sql.Dataset;
@@ -23,13 +25,25 @@ public class ProcessingData {
 						+ "FROM selling_order GROUP BY order_date, customer_id ORDER BY orderDate");
 		try {
 			connection.createStatement();
+
+			String tableName = "user_statistics";
+			String tempTableName = tableName + "_" + getTimeString();
+			PreparedStatement stm = connection.prepareStatement("CREATE TABLE " + tempTableName
+					+ "(" +
+					"    id serial PRIMARY KEY NOT NULL," +
+					"    user_id int," +
+					"    order_date timestamp with time zone," +
+					"    spend double precision" +
+					")");
+			stm.execute();
+
 			Encoder<UserStatistic> userStatisticEncoder = Encoders.bean(UserStatistic.class);
 			Dataset<UserStatistic> userStatisticDF = orderStatisticDs.as(userStatisticEncoder);
 			userStatisticDF.foreach(new ForeachFunction<UserStatistic>() {
 
 				@Override
 				public void call(UserStatistic userStatistic) throws Exception {
-					String query = String.format("INSERT INTO user_statistics(%s) VALUES(%s)",
+					String query = String.format("INSERT INTO " + tempTableName + "(%s) VALUES(%s)",
 							userStatistic.getColumns(), userStatistic.getArgs());
 					PreparedStatement stm = connection.prepareStatement(query);
 					int i = 0;
@@ -39,6 +53,9 @@ public class ProcessingData {
 					stm.execute();
 				}
 			});
+
+			swapTable(tableName, tempTableName);
+
 			connection.commit();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -52,13 +69,26 @@ public class ProcessingData {
 						+ "GROUP BY order_date, product.store_id ORDER BY orderDate");
 		try {
 			connection.createStatement();
+
+			String tableName = "order_statistics";
+			String tempTableName = tableName + "_" + getTimeString();
+			PreparedStatement stm = connection.prepareStatement("CREATE TABLE " + tempTableName
+					+ "(" +
+					"    id serial PRIMARY KEY NOT NULL," +
+					"    store_id int," +
+					"    order_date timestamp with time zone," +
+					"    revenue bigint," +
+					"    total_order int" +
+					")");
+			stm.execute();
+
 			Encoder<OrderStatistic> orderStatisticEncoder = Encoders.bean(OrderStatistic.class);
 			Dataset<OrderStatistic> orderStatisticDF = orderStatisticDs.as(orderStatisticEncoder);
 			orderStatisticDF.foreach(new ForeachFunction<OrderStatistic>() {
 
 				@Override
 				public void call(OrderStatistic user) throws Exception {
-					String query = String.format("INSERT INTO order_statistics(%s) VALUES(%s)", user.getColumns(),
+					String query = String.format("INSERT INTO " + tempTableName + "(%s) VALUES(%s)", user.getColumns(),
 							user.getArgs());
 					PreparedStatement stm = connection.prepareStatement(query);
 					int i = 0;
@@ -68,6 +98,9 @@ public class ProcessingData {
 					stm.execute();
 				}
 			});
+
+			swapTable(tableName, tempTableName);
+
 			connection.commit();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -79,13 +112,24 @@ public class ProcessingData {
 				+ "FROM selling_order JOIN `user` ON `user`.user_id = selling_order.customer_id");
 		try {
 			connection.createStatement();
+
+			String tableName = "product_ages";
+			String tempTableName = tableName + "_" + getTimeString();
+			PreparedStatement stm = connection.prepareStatement("CREATE TABLE " + tempTableName
+					+ "(" +
+					"    id serial PRIMARY KEY NOT NULL," +
+					"    product_id text," +
+					"    age int" +
+					")");
+			stm.execute();
+
 			Encoder<ProductAge> productAgeEncoder = Encoders.bean(ProductAge.class);
 			Dataset<ProductAge> productAgeDF = userDs.as(productAgeEncoder);
 			productAgeDF.foreach(new ForeachFunction<ProductAge>() {
 
 				@Override
 				public void call(ProductAge productAge) throws Exception {
-					String query = String.format("INSERT INTO product_ages(%s) VALUES(%s)", productAge.getColumns(),
+					String query = String.format("INSERT INTO " + tempTableName + "(%s) VALUES(%s)", productAge.getColumns(),
 							productAge.getArgs());
 					PreparedStatement stm = connection.prepareStatement(query);
 					int i = 0;
@@ -95,6 +139,9 @@ public class ProcessingData {
 					stm.execute();
 				}
 			});
+
+			swapTable(tableName, tempTableName);
+
 			connection.commit();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -106,13 +153,26 @@ public class ProcessingData {
 				"SELECT user_id as id, age, gender, unix_timestamp(creation_date) as createdAt, location FROM user");
 		try {
 			connection.createStatement();
+
+			String tableName = "users";
+			String tempTableName = tableName + "_" + getTimeString();
+			PreparedStatement stm = connection.prepareStatement("CREATE TABLE " + tempTableName
+					+ "(" +
+					"    id serial PRIMARY KEY NOT NULL," +
+					"    age int," +
+					"    gender int, " +
+					"    location text," +
+					"    created_at timestamp WITH time ZONE" +
+					")");
+			stm.execute();
+
 			Encoder<User> userEncoder = Encoders.bean(User.class);
 			Dataset<User> userDF = userDs.as(userEncoder);
 			userDF.foreach(new ForeachFunction<User>() {
 
 				@Override
 				public void call(User user) throws Exception {
-					String query = String.format("INSERT INTO users(%s) VALUES(%s)", user.getColumns(), user.getArgs());
+					String query = String.format("INSERT INTO " + tempTableName + "(%s) VALUES(%s)", user.getColumns(), user.getArgs());
 					PreparedStatement stm = connection.prepareStatement(query);
 					int i = 0;
 					for (Object object : user.getParameters()) {
@@ -121,6 +181,9 @@ public class ProcessingData {
 					stm.execute();
 				}
 			});
+
+			swapTable(tableName, tempTableName);
+
 			connection.commit();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -134,13 +197,27 @@ public class ProcessingData {
 						+ " FROM selling_order GROUP BY order_date, product_id ORDER BY orderDate");
 		try {
 			connection.createStatement();
+
+			String tableName = "selling_items";
+			String tempTableName = tableName + "_" + getTimeString();
+			PreparedStatement stm = connection.prepareStatement("CREATE TABLE " + tempTableName
+					+ "(" +
+					"    id serial PRIMARY KEY NOT NULL," +
+					"    product_id text," +
+					"    quantity int, " +
+					"    revenue bigint, " +
+					"    net_revenue double precision," +
+					"    order_date timestamp with time zone" +
+					")");
+			stm.execute();
+
 			Encoder<SellingItem> categoryEncoder = Encoders.bean(SellingItem.class);
 			Dataset<SellingItem> sellingItemDF = sellingItemDs.as(categoryEncoder);
 			sellingItemDF.foreach(new ForeachFunction<SellingItem>() {
 
 				@Override
 				public void call(SellingItem sellingItem) throws Exception {
-					String query = String.format("INSERT INTO selling_items(%s) VALUES(%s)", sellingItem.getColumns(),
+					String query = String.format("INSERT INTO " + tempTableName + "(%s) VALUES(%s)", sellingItem.getColumns(),
 							sellingItem.getArgs());
 					PreparedStatement stm = connection.prepareStatement(query);
 					int i = 0;
@@ -150,6 +227,9 @@ public class ProcessingData {
 					stm.execute();
 				}
 			});
+
+			swapTable(tableName, tempTableName);
+
 			connection.commit();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -161,13 +241,27 @@ public class ProcessingData {
 				"SELECT product_id as id, name as productName , price, subcategory, category, store_id as storeId FROM product");
 		try {
 			connection.createStatement();
+
+			String tableName = "products";
+			String tempTableName = tableName + "_" + getTimeString();
+			PreparedStatement stm = connection.prepareStatement("CREATE TABLE " + tempTableName
+					+ "(" +
+					"    id text PRIMARY KEY NOT NULL," +
+					"    product_name text," +
+					"    price       bigint," +
+					"    subcategory text," +
+					"    category    text," +
+					"    store_id     int" +
+					")");
+			stm.execute();
+
 			Encoder<Product> categoryEncoder = Encoders.bean(Product.class);
 			Dataset<Product> productDF = productDs.as(categoryEncoder);
 			productDF.foreach(new ForeachFunction<Product>() {
 
 				@Override
 				public void call(Product product) throws Exception {
-					String query = "INSERT INTO products(id, product_name, price, subcategory, category, store_id) VALUES(?,?,?,?,?,?)";
+					String query = "INSERT INTO " + tempTableName + "(id, product_name, price, subcategory, category, store_id) VALUES(?,?,?,?,?,?)";
 					PreparedStatement stm = connection.prepareStatement(query);
 					int i = 0;
 					for (Object object : product.getParameters()) {
@@ -176,6 +270,9 @@ public class ProcessingData {
 					stm.execute();
 				}
 			});
+
+			swapTable(tableName, tempTableName);
+
 			connection.commit();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -186,22 +283,44 @@ public class ProcessingData {
 		Dataset<Row> categories = spark.sql("SELECT DISTINCT category as categoryName FROM product");
 		try {
 			connection.createStatement();
+
+			String tableName = "categories";
+			String tempTableName = tableName + "_" + getTimeString();
+			PreparedStatement stm = connection.prepareStatement("CREATE TABLE " + tempTableName
+					+ "(id serial PRIMARY KEY NOT NULL, category_name text)");
+			stm.execute();
+
 			Encoder<Category> categoryEncoder = Encoders.bean(Category.class);
 			Dataset<Category> categoryDF = categories.as(categoryEncoder);
 			categoryDF.foreach(new ForeachFunction<Category>() {
-
 				@Override
 				public void call(Category t) throws Exception {
-					String query = "INSERT INTO categories(category_name) VALUES(?)";
+                    // TODO create temp table
+					String query = "INSERT INTO " + tempTableName + "(category_name) VALUES(?)";
 					PreparedStatement stm = connection.prepareStatement(query);
 					stm.setString(1, t.categoryName);
 					stm.execute();
 				}
 			});
+
+            swapTable(tableName, tempTableName);
+
 			connection.commit();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static String getTimeString() {
+		return new SimpleDateFormat("ddMMyyHHmmss").format(new Date());
+	}
+
+	private static void swapTable(String tableName, String tempTableName) throws SQLException {
+        PreparedStatement stm;
+		stm = connection.prepareStatement("ALTER TABLE " + tableName + " RENAME TO " + tableName + "_bak_" + getTimeString());
+		stm.execute();
+		stm = connection.prepareStatement("ALTER TABLE " + tempTableName + " RENAME TO " + tableName);
+		stm.execute();
 	}
 
 	public static void main(String[] args) {
